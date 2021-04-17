a852b4b5fad1263b6abb58ac89d9f9561cc950bc
panhongchao <panhongchao@yoozoo.com>
2021-04-17 17:55:26 +0800

    feat:1.0.0:afdsfdafadfdafdsfasdfsdaad

--- git-hooks/pre-commit-mac.sh
+++ git-hooks/pre-commit-mac.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
 SCRIPT_PATH=`pwd`
-${SCRIPT_PATH}/gradlew -PcheckCommit="true" checkstyle
+#${SCRIPT_PATH}/gradlew -PcheckCommit="true" checkstyle
+#if [ $? -ne 0 ]; then
+#    echo "checkstyle FAILED, please read the codestyle report."
+#    exit 1
+#else
+#    echo "checkstyle OK!"
+#fi
+
+${SCRIPT_PATH}/gradlew -PcheckPmd="true" pmd
 if [ $? -ne 0 ]; then
-    echo "checkstyle FAILED, please read "
+    echo "pmd check FAILED, please read the pmd report."
     exit 1
 else
-    echo "checkstyle OK!"
+    echo "pmd OK!"
 fi
