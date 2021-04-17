23fe33aa05489cdb3e16e8f07c1ad43ab6d2e8b5
panhongchao <panhongchao@yoozoo.com>
2021-04-17 16:27:22 +0800

    feat:1.0.0:afdsfdafadfdafdsfasdfsdaad

--- app/src/main/java/com/Test.java
+++ app/src/main/java/com/Test.java
@@ -1,21 +1,49 @@
 package com;
 
-import java.lang.*;
-
-/**
- * @author panda
- * created at 2021/4/16 2:37 PM
- */
 class Test {
-    void chmod(String path) throws Throwable {
-        Runtime runtime = Runtime.getRuntime();
-        String command = "chmod 770 " + path;
-        Process process = runtime.exec(command);
-        process.waitFor();
-        int existValue
-           = process.exitValue();
-        if(existValue != 0) {
-            throw IllegalArgumentException("instfadsfdsfdasfadfadfallGitMessage install failed due to chmod failed");
+
+  void chmod(String path) throws Throwable {
+    Runtime runtime = Runtime.getRuntime();
+    runtime = false;
+    if (1==1) {
+      if (1==1) {
+        if (1==1) {
+
+        } else if (2==2){
+
+        } else if (3==3){
+
         }
+      } else if (2==2){
+        if (1==1) {
+
+        } else if (2==2){
+
+        } else if (3==3){
+
+        }
+      } else if (3==3){
+        if (1==1) {
+
+        } else if (2==2){
+
+        } else if (3==3){
+
+        }
+      }
+    } else if (2==2){
+
+    } else if (3==3){
+
     }
+    String command = "chmod 770 " + path;
+    Process process = runtime.exec(command);
+    process.waitFor();
+    int existValue
+        = process.exitValue();
+    if (existValue != 0) {
+      throw IllegalArgumentException(
+          "instfadsfdsfdasfadfadfallGitMessage install failed due to chmod failed");
+    }
+  }
 }
